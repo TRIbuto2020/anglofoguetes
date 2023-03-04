fec0c9599584fc4ddd283716a66b68c8d2e79064
Vitor RS <vitor2000rs@gmail.com>
2023-03-04 19:53:28 -0300

    .

--- src/components/Sobre/index.tsx
+++ src/components/Sobre/index.tsx
@@ -2,7 +2,7 @@ import './Sobre.css'
 
 const Sobre = () => (
     <div className="sobre">
-        <img src="public\AngloRockets.png" alt="" />
+        <img src="src/assets/icons/AngloRockets.png" alt="" />
         <div>
             <h2 className="sobre__titulo">Sobre</h2>
             <p className="sobre__text">Essa é a página do time Anglo para a <a className='sobre__text__highlight' target='_blank' href="http://www.oba.org.br/site/index.php/zerouminforma.com.br/estudante-da-escola-do-sesi-de-dourados-recebe-medalha-de-ouro-na-olimpiada-brasileira-de-astronomia-e-astronautica/?p=conteudo&idcat=29&pag=conteudo">Mostra Brasileira de foguetes (MOBFOG)</a>, uma olímpiada experimental de lançamento de foguetes! Acompanhe o desenvolvimento dos nossos foguetes, bases, nossos testes e lançamentos!</p>
