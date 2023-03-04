f8f03dee700f1de667aa9287d73bd81fec674393
Vitor RS <vitor2000rs@gmail.com>
2023-03-04 19:45:53 -0300

    Seção Sobre

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Regras from './components/Regras'
+import Sobre from './components/Sobre'
 
 function App() {
   return (
     <div className="App">
+      <Sobre />
       <Regras />
     </div>
   )
